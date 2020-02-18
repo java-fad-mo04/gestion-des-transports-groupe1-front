22dd96a93dd2563b5ee8e808d6a8861f507705a7
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2020-02-18 09:44:02 +0100

    Collab - Menu #3

---
+++ src/app/annonces-collab/annonces-collab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-annonces-collab',
+  templateUrl: './annonces-collab.component.html',
+  styles: []
+})
+export class AnnoncesCollabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/stat-collab/stat-collab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-collab',
+  templateUrl: './stat-collab.component.html',
+  styles: []
+})
+export class StatCollabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
