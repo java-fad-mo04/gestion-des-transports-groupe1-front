ddbdfa684ad7ba13d4267cc476cc9a5b2685c84e
Matt34000 <matthieu-le-roy@gmx.fr>
2020-02-24 00:07:24 +0100

    lundi 24/02 00h07

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { FormsModule } from '@angular/forms';
 import { StatutConnecteService } from './auth/statut-connecte.service';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { MenuComponent } from './menu/menu.component';
-import { ReservationsComponent } from './reservations/reservations.component';
+import { ReservationsComponent } from './reservations/reservations.component'; // Matt : USGDT004 - Collab - liste resa
 import { AnnoncesComponent } from './annonces/annonces.component';
 import { StatistiquesComponent } from './statistiques/statistiques.component';
 import { from } from 'rxjs';
@@ -19,15 +19,17 @@ import { VehiculesComponent } from './vehicules/vehicules.component';
 import { PlanningComponent } from './planning/planning.component';
 import { OccupationComponent } from './occupation/occupation.component';
 import { ChauffeursComponent } from './chauffeurs/chauffeurs.component';
+import { ReserverComponent } from './reserver/reserver.component'; // Matt : USGDT012 - Admin - Geolocalisation des véhicules
 
 const routes: Routes = [
   { path: 'tech', component: TechComponent, canActivate: [StatutConnecteService] }, // /tech accessible uniquement si connecté
   { path: 'connexion', component: AuthComponent },
-  { path: 'collaborateur/reservations', component: ReservationsComponent },
+  { path: 'collaborateur/reservations', component: ReservationsComponent }, // Matt : USGDT004 - Collab - Liste des réservations covoiturage
   { path: 'collaborateur/annonces', component: AnnoncesComponent },
   { path: 'collaborateur/statistiques', component: StatistiquesComponent },
   { path: 'admin/chauffeurs', component: ChauffeursComponent },
   { path: 'admin/vehicules', component: VehiculesComponent },
+  { path: 'admin/vehicules/geolocalisation', component: ReserverComponent }, // Matt : USGDT012 - Admin - Geolocalisation des véhicules
   { path: 'chauffeur/occupation', component: OccupationComponent },
   { path: 'chauffeur/planning', component: PlanningComponent },
   { path: '', redirectTo: '/auth', pathMatch: 'full' }
@@ -40,13 +42,14 @@ const routes: Routes = [
     TechComponent,
     AuthComponent,
     MenuComponent,
-    ReservationsComponent,
+    ReservationsComponent, // Matt : USGDT012
     AnnoncesComponent,
     StatistiquesComponent,
     VehiculesComponent,
     PlanningComponent,
     OccupationComponent,
-    ChauffeursComponent
+    ChauffeursComponent,
+    ReserverComponent // Matt : USGDT012
   ],
   imports: [
     BrowserModule,
--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -5,7 +5,7 @@ import { Reservation } from '../models/Reservation';
 import { Collegue } from '../models/Collegue';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { NgbActiveModal, NgbModal, NgbAccordion, NgbPanel } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reservations',
@@ -37,11 +37,60 @@ export class ReservationsComponent implements OnInit {
       map(
         resa => resa.filter( r => new Date(r.date).getTime() < Date.now() )));
 
-
-
-
   }
 
+}
+
+@Component({
+  selector: 'ngbd-modal-confirm',
+  template: `
+  <div class="modal-header">
+    <h4 class="modal-title" id="modal-title">Annuler une réservation</h4>
+    <button type="button" class="close" aria-describedby="modal-title" (click)="modal.dismiss('Cross click')">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <p><strong>Etes-vous sûr de vouloir annuler cette réservation ?</strong></p>
+    <p>
+    <span class="text-danger">Cette réservation sera définitivement annulée !</span>
+    </p>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-outline-secondary" (click)="modal.dismiss('cancel click')">Annuler</button>
+    <button type="button" class="btn btn-danger" (click)="modal.close('Ok click')">Confirmer</button>
+  </div>
+  `
+})
+export class NgbdModalConfirm {
+  constructor(public modal: NgbActiveModal) {}
+}
 
 
+
+const MODALS = {
+  focusFirst: NgbdModalConfirm
+};
+
+@Component({
+  selector: 'ngbd-modal-focus',
+  templateUrl: './modal-focus.html'
+})
+export class NgbdModalFocus {
+  withAutofocus = `<button type="button" ngbAutofocus class="btn btn-danger"
+      (click)="modal.close('Ok click')">Ok</button>`;
+
+  constructor(private _modalService: NgbModal) {}
+
+  open(name: string) {
+    this._modalService.open(MODALS[name]);
+  }
+}
+
+
+@Component({
+  selector: 'ngbd-accordion-static',
+  templateUrl: 'accordion-static.html'
+})
+export class NgbdAccordionStatic {
 }
